3d15d4b9ec46c589dfa0468df23562049d69be53
jasna84 <30408837+jasna84@users.noreply.github.com>
2017-12-29 16:06:12 +0100

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'facebookAuth': {
+        'clientID': '1968447803404591',
+        'clientSecret': '24c375b88130968ca58cd76e52a278d8',
+        'callbackURL': 'http://localhost:3000/home/login/facebook/callback'
+    },
+
+    'googleAuth': {
+        'clientID': 'enter client id here',
+        'clientSecret': 'enter client secret here',
+        //'callbackURL': 'http://localhost:3000/home/login/google/callback'
+    }
+}
